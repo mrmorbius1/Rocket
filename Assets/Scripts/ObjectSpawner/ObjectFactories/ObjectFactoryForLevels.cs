27def8bc252f2d2756057d0f361821af1fbadd0c
Ilya Gavrish <mrmorbius1@gmail.com>
2021-08-27 21:21:43 +0300

    Создание уровней абстракции для Spawner

---
+++ Assets/Scripts/ObjectSpawner/ObjectFactories/ObjectFactoryForLevels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectSpawner.ObjectFactories
+{
+    public abstract class ObjectFactoryForLevels
+    {
+
+    }
+}
